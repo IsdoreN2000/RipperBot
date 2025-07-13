66479624c58a2be53a52c402e3f9bc34ecece8f1
IsdoreN2000 <nwachukwuisdore@gmail.com>
2025-07-13 12:31:23 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import os
 import time
 import requests
 import json
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from telegram import Bot
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
